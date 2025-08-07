c72e42b8288805c1ee6a707f35a117a48d7b0cab
ayush-keshari <ayushkeshari38@gmail.com>
2025-08-08 02:37:23 +0530

    updated

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 module.exports = {
-    MongoURI: 'mongodb+srv://beastayush6969:FqOqECNEIwAs8HNv@cluster0.4rzjzfg.mongodb.net/busbooking?retryWrites=true&w=majority&appName=Cluster0'
+    MongoURI: `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.4rzjzfg.mongodb.net/busbooking?retryWrites=true&w=majority&appName=Cluster0`
 }
