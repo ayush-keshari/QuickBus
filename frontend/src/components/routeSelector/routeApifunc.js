fa93e5b47f48aa6e9eccd33b4b017ff83ba8df52
ayush-keshari <ayushkeshari38@gmail.com>
2025-08-08 21:20:02 +0530

    updated

--- frontend/src/components/routeSelector/routeApifunc.js
+++ frontend/src/components/routeSelector/routeApifunc.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export async function getRoutesFromApi(startCity, destination) {
-    const baseURL = "http://localhost:8080/booking/search"; // ✅ correct endpoint
+    const baseURL = "https://quick-bus.vercel.app/booking/search"; // ✅ correct endpoint
     let response = await axios.post(baseURL, { startCity, destination });
     return response;
 }
